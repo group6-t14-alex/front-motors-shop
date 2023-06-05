a31bbc863fbedcd3aad2b9d7267b6687ba7fb406
Fernanda Bollinger <feernanda.bollinger@gmail.com>
2023-06-05 16:13:36 -0300

    feat: add page

---
+++ src/pages/productPage.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import {GiHamburgerMenu} from "react-icons/gi"
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuItemOption,
+    MenuGroup,
+    MenuOptionGroup,
+    MenuDivider,
+    IconButton,
+    Avatar,
+    Text,
+    Box,
+} from '@chakra-ui/react'
+
+import logo from "../assets/logoMotors.png"
+
+const ProductPage = () => {
+
+
+    return (
+        <>
+
+        </>
+    )
+
+}
+
+export default ProductPage
