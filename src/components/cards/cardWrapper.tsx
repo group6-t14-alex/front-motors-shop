13999f8bd4ebb82d6fcd5989d6c9adef49bdb1f8
samirdourado <samir3500@gmail.com>
2023-06-06 16:29:18 -0300

    Fix: Card promo

--- src/components/cards/cardWrapper.tsx
+++ src/components/cards/cardWrapper.tsx
@@ -4,14 +4,15 @@ import { wrap } from 'module'
 
 
 const CardWrapper = () => {
+
     return (
         <UnorderedList maxWidth='1032px' display='flex' flexWrap='wrap' justifyContent='space-between' margin='0 auto'>
-            <CardUser carName={'Maserati - Ghibli'}/>
-            <CardUser carName={'Brasília'}/>
-            <CardUser carName={'Fusca'}/>
-            <CardUser carName={'Opala'}/>
-            <CardUser carName={'Kadet'}/>
-            <CardUser carName={'Passat'}/>
+            <CardUser carName={'Maserati - Ghibli'} price={10000} fipePrice={6000}/>
+            <CardUser carName={'Brasília'} price={2000} fipePrice={5000}/>
+            <CardUser carName={'Fusca'} price={5000} fipePrice={4000}/>
+            <CardUser carName={'Opala'} price={4000} fipePrice={8000}/>
+            <CardUser carName={'Kadet'} price={5000} fipePrice={2000}/>
+            <CardUser carName={'Passat'} price={6000} fipePrice={7000}/>
         </UnorderedList>
     )
 }
