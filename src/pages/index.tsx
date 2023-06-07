0817af036c0263c3304235b77898a1976e5f59f7
Lilian Fernandes Oliveira Dias <biologia.lilianfernandes@gmail.com>
2023-06-07 16:02:29 -0300

    feat:footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import CardWrapper from "@/components/cards/cardWrapper";
+import { Footer } from "@/components/footer/footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -14,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <CardWrapper />
+        <Footer />
       </main>
     </>
   );
