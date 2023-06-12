9edb4f0be1e02ad120a6acb63ddbbbb8c955a981
eskokado <eskokado@gmail.com>
2023-06-12 14:21:40 -0300

    feat: create signup

---
+++ src/pages/signup.tsx
@@ -0,0 +1,16 @@
+"use client";
+import {Header} from "@/components/header/header";
+import {Footer} from "@/components/footer/footer";
+import SignupCard from "@/components/cards/signupCard";
+
+const Signup = () => {
+  return (
+    <>
+      <Header />
+      <SignupCard />
+      <Footer />
+    </>
+  )
+}
+
+export default Signup
