e7d3b9feadd636967d9aa6f93d7c1306db55658d
eskokado <eskokado@gmail.com>
2023-06-14 11:39:36 -0300

    feat: integration authContext in SignupCard

--- src/components/cards/signupCard.tsx
+++ src/components/cards/signupCard.tsx
@@ -12,15 +12,35 @@ import {
   Heading,
   Text,
   useColorModeValue,
-  Link, Textarea,
+  Link, Textarea, FormErrorMessage,
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import {useForm} from "react-hook-form";
+import {UserRequest, userSchemaRequest} from "@/schemas/user.schema";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useAuth} from "@/contexts/authContext";
 
 export default function SignupCard() {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [typeUser, setTypeUser] = useState('buyer');
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<UserRequest>({
+    resolver: zodResolver(userSchemaRequest)
+  });
+
+  const { registerUser  } = useAuth();
+
+  const onFormSubmit = (formData: UserRequest) => {
+    console.log(formData);
+    formData.type_user = typeUser;
+    registerUser(formData);
+  };
 
   return (
     <Flex
@@ -28,7 +48,7 @@ export default function SignupCard() {
       align={'center'}
       justify={'center'}
       bg={'grey9'}>
-      <form >
+      <form onSubmit={handleSubmit(onFormSubmit)}>
         <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
           <Box
             rounded={'lg'}
@@ -44,66 +64,126 @@ export default function SignupCard() {
               </Text>
             </Stack>
             <Stack spacing={4}>
-              <FormControl id="name" isRequired>
+              <FormControl id="name" isInvalid={Boolean(errors.name)}>
                 <FormLabel>Nome</FormLabel>
-                <Input type="text" placeholder={'Ex. Samuel Leão'}/>
+                <Input
+                  type="text"
+                  placeholder={'Ex. Samuel Leão'}
+                  {...register("name")}
+                />
+                <FormErrorMessage>
+                  {errors.name && errors.name.message}
+                </FormErrorMessage>
               </FormControl>
-              <FormControl id="email" isRequired>
+              <FormControl id="email" isInvalid={Boolean(errors.email)}>
                 <FormLabel>Email</FormLabel>
-                <Input type="email" placeholder={'Ex: samuel@kenzie.com.br'} />
+                <Input
+                  type="email"
+                  placeholder={'Ex: samuel@kenzie.com.br'}
+                  {...register("email")}
+                />
+                <FormErrorMessage>
+                  {errors.email && errors.email.message}
+                </FormErrorMessage>
               </FormControl>
-              <FormControl id="cpf" isRequired>
+              <FormControl id="cpf" isInvalid={Boolean(errors.cpf)}>
                 <FormLabel>CPF</FormLabel>
-                <Input type="text" placeholder={'000.000.000-00'} />
+                <Input
+                  type="text"
+                  placeholder={'000.000.000-00'}
+                  {...register("cpf")}
+                />
+                <FormErrorMessage>
+                  {errors.cpf && errors.cpf.message}
+                </FormErrorMessage>
               </FormControl>
-              <FormControl id="phone" isRequired>
+              <FormControl id="phone">
                 <FormLabel>Celular</FormLabel>
-                <Input type="text" placeholder={'(DDD) 90000-0000'}/>
+                <Input
+                  type="text"
+                  placeholder={'(DDD) 90000-0000'}
+                  {...register("phone")}
+                />
               </FormControl>
-              <FormControl id="birthDate" isRequired>
+              <FormControl id="birthDate">
                 <FormLabel>Data de Nascimento</FormLabel>
-                <Input type="date" placeholder={'00/00/0000'}/>
+                <Input
+                  type="date"
+                  placeholder={'00/00/0000'}
+                  {...register("date_of_birth")}
+                />
               </FormControl>
-              <FormControl id="description" isRequired>
+              <FormControl id="description">
                 <FormLabel>Descrição</FormLabel>
-                <Textarea placeholder={'Digitar descrição'}/>
+                <Textarea
+                  placeholder={'Digitar descrição'}
+                  {...register("description")}
+                />
               </FormControl>
               <Text fontSize={'body1'} as={'b'} pb={'1.5rem'} pt={'1.5rem'}>
                 Informações de endereço️
               </Text>
-              <FormControl id="cep" isRequired>
+              <FormControl id="cep" isInvalid={Boolean(errors.cep)}>
                 <FormLabel>CEP</FormLabel>
-                <Input placeholder={'00000-000'}/>
+                <Input
+                  placeholder={'00000-000'}
+                  {...register("cep")}
+                />
+                <FormErrorMessage>
+                  {errors.cep && errors.cep.message}
+                </FormErrorMessage>
               </FormControl>
               <HStack>
                 <Box>
-                  <FormControl id="state" isRequired>
+                  <FormControl id="state" isReadOnly={true}>
                     <FormLabel>Estado</FormLabel>
-                    <Input type="text" placeholder={'Digitar Estado'} />
+                    <Input
+                      type="text"
+                      placeholder={'Digitar Estado'}
+                      {...register("state")}
+                    />
                   </FormControl>
                 </Box>
                 <Box>
-                  <FormControl id="city">
+                  <FormControl id="city" isReadOnly={true}>
                     <FormLabel>Cidade</FormLabel>
-                    <Input type="text" placeholder={'Digitar cidade'} />
+                    <Input
+                      type="text"
+                      placeholder={'Digitar cidade'}
+                      {...register("city")}
+                    />
                   </FormControl>
                 </Box>
               </HStack>
-              <FormControl id="address" isRequired>
+              <FormControl id="address" isReadOnly={true}>
                 <FormLabel>Rua</FormLabel>
-                <Input placeholder={'Digitar logradouro'}/>
+                <Input
+                  placeholder={'Digitar logradouro'}
+                  {...register("address")}
+                />
               </FormControl>
               <HStack>
                 <Box>
-                  <FormControl id="number" isRequired>
+                  <FormControl id="number" isInvalid={Boolean(errors.number)}>
                     <FormLabel>Número</FormLabel>
-                    <Input type="text" placeholder={'Digitar número'} />
+                    <Input
+                      type="text"
+                      placeholder={'Digitar número'}
+                      {...register("number")}
+                    />
+                    <FormErrorMessage>
+                      {errors.number && errors.number.message}
+                    </FormErrorMessage>
                   </FormControl>
                 </Box>
                 <Box>
                   <FormControl id="complement">
                     <FormLabel>Complemento</FormLabel>
-                    <Input type="text" placeholder={'Ex: apto 3307'} />
+                    <Input
+                      type="text"
+                      placeholder={'Ex: apto 3307'}
+                      {...register("complement")}
+                    />
                   </FormControl>
                 </Box>
               </HStack>
@@ -136,10 +216,14 @@ export default function SignupCard() {
                   Anunciante
                 </Button>
               </Stack>
-              <FormControl id="password" isRequired>
+              <FormControl id="password" isInvalid={Boolean(errors.password)}>
                 <FormLabel>Senha</FormLabel>
                 <InputGroup>
-                  <Input type={showPassword ? 'text' : 'password'} placeholder={'Digitar senha'} />
+                  <Input
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder={'Digitar senha'}
+                    {...register("password")}
+                  />
                   <InputRightElement h={'full'}>
                     <Button
                       variant={'ghost'}
@@ -150,11 +234,18 @@ export default function SignupCard() {
                     </Button>
                   </InputRightElement>
                 </InputGroup>
+                <FormErrorMessage>
+                  {errors.password && errors.password.message}
+                </FormErrorMessage>
               </FormControl>
-              <FormControl id="confirmPassword" isRequired>
+              <FormControl id="confirmPassword" isInvalid={Boolean(errors.confirmPassword)}>
                 <FormLabel>Confirmar Senha</FormLabel>
                 <InputGroup>
-                  <Input type={showConfirmPassword ? 'text' : 'password'} placeholder={'Confirmar senha'}/>
+                  <Input
+                    type={showConfirmPassword ? 'text' : 'password'}
+                    placeholder={'Confirmar senha'}
+                    {...register("confirmPassword")}
+                  />
                   <InputRightElement h={'full'}>
                     <Button
                       variant={'ghost'}
@@ -165,6 +256,9 @@ export default function SignupCard() {
                     </Button>
                   </InputRightElement>
                 </InputGroup>
+                <FormErrorMessage>
+                  {errors.confirmPassword && errors.confirmPassword.message}
+                </FormErrorMessage>
               </FormControl>
               <Stack spacing={10} pt={2}>
                 <Button
@@ -174,6 +268,7 @@ export default function SignupCard() {
                   color={'white'}
                   _hover={{ bg: "brand2"}}
                   _focus={{ bg: "brand1", color: "white"}}
+                  type="submit"
                 >
                   Finalizar cadastro
                 </Button>
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -2,8 +2,10 @@
 import {Header} from "@/components/header/header";
 import {Footer} from "@/components/footer/footer";
 import SignupCard from "@/components/cards/signupCard";
+import {NextPage} from "next";
+import {UserRequest} from "@/schemas/user.schema";
 
-const Signup = () => {
+const Signup: NextPage = () => {
   return (
     <>
       <Header />
