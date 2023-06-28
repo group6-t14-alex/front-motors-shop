ae36f2af1a7e702ea5513d40a4adc7017530234e
eskokado <eskokado@gmail.com>
2023-06-28 16:40:09 -0300

    feat: create card comments

--- src/components/cards/commentsCard.tsx
+++ src/components/cards/commentsCard.tsx
@@ -1,8 +1,20 @@
 import { Avatar, Box, Heading, List, ListItem, Text, UnorderedList } from '@chakra-ui/react'
 import CommentInputCard from './commentInputCard'
 import CommentItemCard from './commentItemCard'
+import { useUser } from '@/contexts/userContext'
+import { useAuth } from '@/contexts/authContext'
+import { useCommentContext } from '@/contexts/commentsContext'
+import { useEffect } from 'react'
+import { api } from '@/services/api'
 
-const CommentsCard = ({ userList, runningTime }: any) => {
+const CommentsCard = ({carId} : any) => {
+  const { comments } = useCommentContext();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    // console.log(user);
+  }, [user])
+
   return (
     <Box display='flex' flexDirection='column' w={{cel:"95%", desk:'50%'}}>
     <Box h='845px'  w='100%'  maxWidth='1032px' borderRadius='4px' p='36px 28px' bg='grey10' marginTop='18px' display='flex' flexDirection='column' gap='13px'>
@@ -10,13 +22,15 @@ const CommentsCard = ({ userList, runningTime }: any) => {
 
         <List h='724px' maxH='724px' display='flex' flexDirection='column' alignItems='flex-start' gap='44px' overflowY='auto'>
 
-          <CommentItemCard name={'Júlia Lima'} runningTime={runningTime} comment={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} />
-          <CommentItemCard name={'Marcos Antônio'} runningTime={runningTime} comment={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} />
-          <CommentItemCard name={'Camila Silva'} runningTime={runningTime} comment={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} />
-          <CommentItemCard name={'Júlia Lima'} runningTime={runningTime} comment={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} />
+          {user && user.comments.map((comment: any) => {
+            return (
+                <CommentItemCard key={comment.id} name={user!.name} comment={comment} />
+              );
+            })
+          }
         </List>
     </Box>
-    <CommentInputCard />
+    <CommentInputCard carId={carId}/>
   </Box>
 
   )
